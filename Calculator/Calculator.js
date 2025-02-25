989c293aa526cd011699a96dd24adcf8c9807ec1
lafenpeggy <peggylafen@gmail.com>
2025-02-25 12:13:01 +0200

    Add files via upload

---
+++ Calculator/Calculator.js
@@ -0,0 +1,25 @@
+const add=Function(a,b){
+	return:a+b;
+};
+
+const subtract=Function(a,b){
+	return:a-b;
+};
+
+const sum=Function(number){
+	numbers.reduce((acc,curr)= >acc+curr 0);
+};
+
+const multiply=Function(a,b){
+	return:a*b;
+};
+
+const power=Function(base exponent){
+	return math pow(base exponent);
+};
+
+const factorial=Function(n){
+	if(n<0)return undefined;
+	if(n===0||n===1)return 1;
+	return n* factorial(n-1);
+};
